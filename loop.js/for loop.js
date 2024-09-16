fa174038d32b9a8a6d69bb22fe318257e1dd4a8b
pheonixX2 <ahmedsaif54401@gmail.com>
2024-09-16 12:00:57 +0500

    well

---
+++ loop.js/for loop.js
@@ -0,0 +1,61 @@
+// for  loops function
+// syntax for ( <iniatialization> ; <<<stopping_condition>; <function ;)
+
+// for (let count = 1; count <= 15; count++ ){
+//     console.log("Hello world");
+//     }
+// console.log("hello world");
+
+// let sum = 0;
+
+// for ( i = 1 ; i <= 5; i++){
+//     sum += i
+//     }
+
+//     console.log(sum);
+
+// let sum = 0;
+// let n = 30;
+
+// for ( i = 1; i <= n; i++ ){
+//     sum+= n
+// }
+
+// console.log(sum)
+
+
+
+// for (i = 1; i >= 0; i++){
+//     console.log("test");
+// }
+
+
+// let i = 1;
+// while(i <= 50){
+//     console.log("hello");
+//     i++;
+// }
+// let i = 1
+
+// do{
+//     console.log("test");
+//     i++;
+// // }while(i <= 20)
+// let str = "Apna college"
+// for(let i of str){
+//     console.log(i);
+// }
+
+
+let list = {
+    test: true,
+    title: "title",
+    branch: "main"
+}
+
+
+
+for (let i in list){
+    console.log(i , i.i);
+}
+
