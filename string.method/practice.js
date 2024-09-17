71a059571fcfda86a3654cb14f7ce5435f4d38c4
pheonixX2 <ahmedsaif54401@gmail.com>
2024-09-17 11:00:38 -0700

    more

---
+++ string.method/practice.js
@@ -0,0 +1,13 @@
+
+// let username = prompt("Enter your username");
+// let Number = "17";
+// let Symbol = "@"
+// alert(Symbol + username.trim() + Number).toUpperCase();
+
+let username = prompt("Enter your username");
+let num = "17";
+let name1 = username.concat(num);
+let name = "@";
+let res = name.concat(name1);
+console.log("username is  ",res);
+alert("username is  ", res)
--- string.method/string_method.js
+++ string.method/string_method.js
@@ -1,5 +1,14 @@
-let str = "tigeress";
+let str = "   tigeress test";
+let str2 = 'hello'
 console.log(str.length);
 console.log(str[1]);
 
 console.log(`this a template  ${str} literal `);
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.trim());
+console.log(str);// removes white spaces
+console.log(str2.slice(1,4));
+console.log(str.concat(str2));
+console.log(str.replace("ige","123"));
+console.log(str[5]);
